d9b96e99a5f6901a5678a31412962f126bf2ba53
Vu Duc Tuyen <tuyen@naxam.net>
2017-05-29 00:12:40 +0700

    OkHttp3 v3.8.0

--- Naxam.SquareUp.OkHttp3/Properties/AssemblyInfo.cs
+++ Naxam.SquareUp.OkHttp3/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using Android.App;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Naxam.SquareUp.OkHttp3")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Xamarin Binding Library - SquareUp OkHttp3")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("NAXAM CO.,LTD")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyCompany("NAXAM COMPANY LIMITED")]
+[assembly: AssemblyProduct("X Bindings")]
 [assembly: AssemblyCopyright("Copyright (c) 2017 NAXAM")]
 [assembly: AssemblyTrademark("NAXAM")]
 [assembly: AssemblyCulture("")]
@@ -18,8 +18,8 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("3.7.0")]
-[assembly: AssemblyInformationalVersion("3.7.0")]
+[assembly: AssemblyVersion("3.8.0")]
+[assembly: AssemblyInformationalVersion("3.8.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
